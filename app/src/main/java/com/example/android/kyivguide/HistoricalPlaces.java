00b884b5e997bc23ac5979b2fcaadca59e8f3db0
Enver Ablaiev <taraqtash@gmail.com>
2017-04-04 12:57:17 +0300

    Created a PlaceAdapter and filled it with some historical places of Kyiv

--- app/src/main/java/com/example/android/kyivguide/HistoricalPlaces.java
+++ app/src/main/java/com/example/android/kyivguide/HistoricalPlaces.java
@@ -18,8 +18,11 @@ public class HistoricalPlaces extends AppCompatActivity {
         places.add(new Place(R.drawable.kostel, "Kostel", "Kostel"));
         places.add(new Place(R.drawable.maidanezalezhnosti, "Independence Square", "The Central Square of Ukraine"));
         places.add(new Place(R.drawable.zolotievorota, "Golden Gates", "The Entry to the Old City of Kyiv"));
-//        places.add(new Place(R.drawable.mihaylovskaya, "St.Michael Church", "St.Michael"));
-//        places.add(new Place(R.drawable.motherland, "The Motherland Statue", "The Motherland Statue"));
+        places.add(new Place(R.drawable.motherland, "The Motherland Statue", "The Motherland Statue"));
+        places.add(new Place(R.drawable.kostel, "Khreshchattyk Street", "The main street of Ukraine"));
+        places.add(new Place(R.drawable.kostel, "Kyiv Pechersk Lavra", "A historic Orthodox Christian " +
+                "monastery which gave its name to one of the city districts where it is located in Kiev"));
+
 
         // Create a {@link PlaceAdapter}, whose data source is a list of {@link Place}s
         PlaceAdapter adapter = new PlaceAdapter(this, places);
